57cf7a1f6348e3ab66c338a38e7275ef12516439
Robert Huitl <git@huitl.de>
2023-02-08 09:37:45 +0100

    Update install.sh
    try to make it build

--- .devcontainer/local-features/setup-user/install.sh
+++ .devcontainer/local-features/setup-user/install.sh
@@ -62,59 +62,23 @@ rm -rf /usr/local/nvs/deps/node_modules/follow-redirects/*
 curl -sSL https://github.com/follow-redirects/follow-redirects/archive/refs/tags/v1.15.2.tar.gz | tar -xzC /tmp 2>&1
 mv /tmp/follow-redirects-1.15.2/*  /usr/local/nvs/deps/node_modules/follow-redirects/
 
-# Enables the oryx tool to generate manifest-dir which is needed for running the postcreate tool
-DEBIAN_FLAVOR="focal-scm"
-mkdir -p /opt/oryx && echo "vso-focal" > /opt/oryx/.imagetype
-echo "DEBIAN|${DEBIAN_FLAVOR}" | tr '[a-z]' '[A-Z]' > /opt/oryx/.ostype
-
-# Oryx expects the tool to be installed at `/opt/oryx` and looks for relevant files in there.
-ln -snf /usr/local/oryx/* /opt/oryx
-
 # For the universal image, oryx build tool installs the detected platforms in /home/codespace/*. Hence, linking current platforms to the /home/codespace/ path and adding it to the PATH.
 # This ensures that whatever platfornm versions oryx detects and installs are set as root.
 NODE_PATH="/home/codespace/nvm/current"
 ln -snf /usr/local/share/nvm /home/codespace
 
-PHP_PATH="/home/${USERNAME}/.php/current"
-mkdir -p /home/${USERNAME}/.php
-ln -snf /usr/local/php/current $PHP_PATH
-
-PYTHON_PATH="/home/${USERNAME}/.python/current"
-mkdir -p /home/${USERNAME}/.python
-ln -snf /usr/local/python/current $PYTHON_PATH
-ln -snf /usr/local/python /opt/python
-
 JAVA_PATH="/home/codespace/java/current"
 ln -snf /usr/local/sdkman/candidates/java /home/codespace
-
-RUBY_PATH="/home/${USERNAME}/.ruby/current"
-mkdir -p /home/${USERNAME}/.ruby
-ln -snf /usr/local/rvm/rubies/default $RUBY_PATH
-
-DOTNET_PATH="/home/${USERNAME}/.dotnet"
-ln -snf /usr/local/dotnet/current $DOTNET_PATH
-mkdir -p /opt/dotnet/lts
-cp -R /usr/local/dotnet/current/dotnet /opt/dotnet/lts
-cp -R /usr/local/dotnet/current/LICENSE.txt /opt/dotnet/lts
-cp -R /usr/local/dotnet/current/ThirdPartyNotices.txt /opt/dotnet/lts
 
 MAVEN_PATH="/home/${USERNAME}/.maven/current"
 mkdir -p /home/${USERNAME}/.maven
 ln -snf /usr/local/sdkman/candidates/maven/current $MAVEN_PATH
-
-HUGO_ROOT="/home/${USERNAME}/.hugo/current"
-mkdir -p /home/${USERNAME}/.hugo
-ln -snf /usr/local/hugo $HUGO_ROOT
 
 HOME_DIR="/home/${USERNAME}/"
 chown -R ${USERNAME}:${USERNAME} ${HOME_DIR}
 chmod -R g+r+w "${HOME_DIR}"
 find "${HOME_DIR}" -type d | xargs -n 1 chmod g+s
 
-OPT_DIR="/opt/"
-chown -R ${USERNAME}:oryx ${OPT_DIR}
-chmod -R g+r+w "${OPT_DIR}"
-find "${OPT_DIR}" -type d | xargs -n 1 chmod g+s
 
 echo "Defaults secure_path=\"${DOTNET_PATH}:${NODE_PATH}/bin:${PHP_PATH}/bin:${PYTHON_PATH}/bin:${JAVA_PATH}/bin:${RUBY_PATH}/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin:/usr/local/share:/home/${USERNAME}/.local/bin:${PATH}\"" >> /etc/sudoers.d/$USERNAME
 
